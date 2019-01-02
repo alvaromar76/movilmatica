6a27a0e78e6a4e6c57edda7d851113d5d8812ce1
alvaromar76 <alvaromar76@hotmail.com>
2019-01-01 19:42:52 -0500

    cambio en intro

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PadreComponent } from './padre/padre.component';
 import { CarruselComponent } from './carrusel/carrusel.component';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
-  declarations: [PadreComponent, CarruselComponent],
+  declarations: [PadreComponent, CarruselComponent, IntroComponent],
   imports: [
     CommonModule
   ],
-  exports :[PadreComponent,CarruselComponent]
+  exports :[PadreComponent,CarruselComponent,IntroComponent]
 })
 export class HomeModule { }
