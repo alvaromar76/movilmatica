e05d4dbf947ff4225f5dfc7b410d79174078b9d4
alvaromar76 <alvaromar76@hotmail.com>
2019-01-03 21:33:54 -0500

    productos

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { PadreComponent } from './padre/padre.component';
 import { CarruselComponent } from './carrusel/carrusel.component';
 import { IntroComponent } from './intro/intro.component';
+import { ProductosComponent } from './productos/productos.component';
 
 @NgModule({
-  declarations: [PadreComponent, CarruselComponent, IntroComponent],
+  declarations: [PadreComponent, CarruselComponent, IntroComponent, ProductosComponent],
   imports: [
     CommonModule
   ],
-  exports :[PadreComponent,CarruselComponent,IntroComponent]
+  exports :[PadreComponent,CarruselComponent,IntroComponent,ProductosComponent]
 })
 export class HomeModule { }
